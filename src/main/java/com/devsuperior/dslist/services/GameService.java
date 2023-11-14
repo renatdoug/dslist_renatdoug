df8326a6e497e881b720360f380446b9877f28d0
Renato Douglas <renatdoug@hotmail.com>
2023-11-14 01:35:40 -0300

    Game repository, service, controller

---
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.devsuperior.dslist.dto.GameMinDTO;
+import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.repositories.GameRepository;
+
+
+// Para registar essa classe como componente, usade Anotation @Component ou @Service
+@Component
+public class GameService {
+
+	@Autowired  //anotation para injetar um componente no outro;
+	private GameRepository gameRepository;
+	
+	public List<GameMinDTO> findAll(){
+		List<Game> result = gameRepository.findAll();
+		List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+		return dto;
+	}
+	
+}
